20d7b0d7a86138f6bb4c5d9ae482c47ecbecbe64
Sajan4072 <rai4072@gmail.com>
2022-05-10 17:45:18 +0545

    customizing token claims

--- backend/base/api/urls.py
+++ backend/base/api/urls.py
@@ -1,5 +1,6 @@
 from django.urls import URLPattern, path
 from . import views
+from .views import MyTokenObtainPairView
 
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
@@ -8,6 +9,6 @@ from rest_framework_simplejwt.views import (
 
 urlpatterns=[
     path('',views.getRoutes),
-    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
